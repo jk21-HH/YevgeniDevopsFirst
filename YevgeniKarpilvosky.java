34d2d8a0565fb55790afecfdbabcf391f15b3a67
jk21-HH <jeanya12@gmail.com>
2022-03-13 21:35:48 +0200

    first commit

---
+++ YevgeniKarpilvosky.java
@@ -0,0 +1,10 @@
+public class YevgeniKarpilvosky
+{
+    public static void main(String[] args)
+    {
+        for(int i=0;i<22;i++)
+        {
+            System.out.println("Yevgeni Karpilovsky");
+        }
+    }
+}
